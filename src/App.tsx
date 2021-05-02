76b827a29af6634adc29b53f7dbeb23b13dcab41
오동웅 <woong@MacPro.local>
2021-05-03 01:08:49 +0900

    Add to Login Service & Lobby component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import ".App.css";
 import AuthServiceImpl from "./components/service/authService";
-import styles from "./App.module.css";
+import postCSS from "./App.module.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Lobby from "./components/Lobby/Lobby";
 
 interface AuthService {
   authService: AuthServiceImpl;
 }
 
 const App: React.FC<AuthService> = ({ authService }) => {
-  return <section className={styles.section}></section>;
+  return (
+    <section className={postCSS.App}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService}></Login>
+          </Route>
+          <Route path="/lobby">
+            <Lobby></Lobby>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </section>
+  );
 };
 
 export default App;
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useHistory } from "react-router";
+import AuthServiceImpl from "../service/authService";
+import postCSS from "./Login.module.css";
+
+interface LoginService {
+  authService: AuthServiceImpl;
+}
+
+type LoginContent = {
+  title: string;
+  gooleBtnName: string;
+  githubBtnName: string;
+};
+
+const Login: React.FC<LoginService> = ({ authService }) => {
+  const history = useHistory();
+
+  const userLoginTextContent: LoginContent = {
+    title: "Login",
+    gooleBtnName: "Google",
+    githubBtnName: "Github",
+  };
+
+  const loginBtnHandler: React.MouseEventHandler<HTMLButtonElement> = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): void => {
+    authService
+      .login(event.currentTarget.textContent! as string)
+      .then((userDB) => {
+        const userId = userDB.user.uid;
+        goToLobby(userId);
+      });
+  };
+
+  const goToLobby = (userId: string): void => {
+    history.push({
+      pathname: "/lobby",
+      state: { id: userId },
+    });
+  };
+
+  return (
+    <div className={postCSS.Login}>
+      <div className={postCSS.title}>{userLoginTextContent.title}</div>
+      <section className={postCSS.btns}>
+        <button onClick={loginBtnHandler} className={postCSS.googleBtn}>
+          <span className={postCSS.googleFont}>
+            {userLoginTextContent.gooleBtnName}
+          </span>
+        </button>
+        <button onClick={loginBtnHandler} className={postCSS.githubBtn}>
+          <span className={postCSS.githubFont}>
+            {userLoginTextContent.githubBtnName}
+          </span>
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
